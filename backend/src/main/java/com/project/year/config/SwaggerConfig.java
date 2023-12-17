6a0265b16ed4a8af5d652146a0dfaa941b4be488
dongyeop <eaea7314@naver.com>
2023-12-17 17:56:15 +0900

    [fix]: Swagger config 설정 오류 수정

--- backend/src/main/java/com/project/year/config/SwaggerConfig.java
+++ backend/src/main/java/com/project/year/config/SwaggerConfig.java
@@ -3,11 +3,22 @@ package com.project.year.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig {
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi user() {
+        String[] packagesToScan = {"com.project.year.user"};
+
+        return GroupedOpenApi.builder()
+                .group("user")
+                .packagesToScan(packagesToScan)
+                .build();
+    }
 
     @Bean
     public OpenAPI openApiConfig() {
