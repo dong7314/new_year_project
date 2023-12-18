e3edbc4aa24751a570fcb9398af681dafb78ebbc
dongyeop <eaea7314@naver.com>
2023-12-18 22:05:38 +0900

    [add]: Spring Security 적용

---
+++ backend/src/main/java/com/project/year/user/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.project.year.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequests) ->
+                    authorizeRequests.requestMatchers(new AntPathRequestMatcher("/api/**")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/api/v1/users/join")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/api/v1/users/login")).permitAll()
+                ).sessionManagement((sessionManagement) ->
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).build();
+    }
+
+}
